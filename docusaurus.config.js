30cf04a7e58586d4287dd7ae9b0e85aa6be054e7
Jay Maverick <jay@debianmuhku.lotankoti>
2026-05-29 05:55:34 +0300

    Fixed Deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,6 +29,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'jaymaverick', // Usually your GitHub org/user name.
   projectName: 'ds-homepage', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
