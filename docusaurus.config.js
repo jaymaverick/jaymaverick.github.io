db65ffdf0f63c6d95695d36dcecdcab0dd2bc0a0
Jay Maverick <jay@debianmuhku.lotankoti>
2026-05-30 20:49:20 +0300

    Title fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Jay Pitkänen',
-  tagline: 'AI Deployment Strategist',
+  tagline: 'AI Solutions Architect',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
